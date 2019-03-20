57ad47a2b997602e1e22e4f6fbb500d62df259b9
Constantine <log.kot@gmail.com>
2019-03-20 05:25:16 +0300

    api doc generator

--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::group(['prefix' => 'v1'], function () {
+Route::group(['prefix' => 'v1', 'namespace' => 'Api\Version1'], function () {
     Route::resource('users', 'UserController');
     Route::resource('companies', 'CompanyController');
 });
